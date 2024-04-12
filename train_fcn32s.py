1a12e332820bb9d8a0c8215a3c1c4391cae02093
ayleen farnood <46452671+ayleenf@users.noreply.github.com>
2024-04-12 19:17:27 -0400

    Add files via upload

---
+++ train_fcn32s.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import os
+import os.path as osp
+import shlex
+import subprocess
+import time
+import torch
+import yaml
+
+import sys
+sys.path.append("C:\MIE424\FCN\pytorch-fcn")
+import torchfcn
+
+
+
+def get_parameters(model, bias=False):
+    import torch.nn as nn
+    modules_skipped = (
+        nn.ReLU,
+        nn.MaxPool2d,
+        nn.Dropout2d,
+        nn.Sequential,
+        torchfcn.models.FCN32s,
+    )
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            if bias:
+                yield m.bias
+            else:
+                yield m.weight
+        elif isinstance(m, nn.ConvTranspose2d):
+            # weight is frozen because it is bilinear upsampling
+            if bias:
+                assert m.bias is None
+        elif isinstance(m, modules_skipped):
+            continue
+        else:
+            raise ValueError('Unexpected module: %s' % str(m))
+
+
+here = osp.dirname(osp.abspath(__file__))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('-g', '--gpu', type=int, required=True, help='gpu id')
+    parser.add_argument('--resume', help='checkpoint path')
+    parser.add_argument(
+        '--max-iteration', type=int, default=100000, help='max iteration'
+    )
+    parser.add_argument(
+        '--lr', type=float, default=1.0e-10, help='learning rate',
+    )
+    parser.add_argument(
+        '--weight-decay', type=float, default=0.0005, help='weight decay',
+    )
+    parser.add_argument(
+        '--momentum', type=float, default=0.99, help='momentum',
+    )
+    args = parser.parse_args()
+
+    args.model = 'FCN32s'
+
+    now = datetime.datetime.now()
+    args.out = osp.join(here, 'logs', now.strftime('%Y%m%d_%H%M%S.%f'))
+
+    os.makedirs(args.out)
+    with open(osp.join(args.out, 'config.yaml'), 'w') as f:
+        yaml.safe_dump(args.__dict__, f, default_flow_style=False)
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+    cuda = torch.cuda.is_available()
+
+    torch.manual_seed(1337)
+    if cuda:
+        torch.cuda.manual_seed(1337)
+
+    # 1. dataset
+
+    root = osp.expanduser('~/data/datasets')
+    kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        torchfcn.datasets.VOC2012ClassSeg(root, split='train', transform=True),
+        batch_size=1, shuffle=True, **kwargs)
+    val_loader = torch.utils.data.DataLoader(
+        torchfcn.datasets.VOC2012ClassSeg( 
+            root, split='val', transform=True),
+        batch_size=1, shuffle=False, **kwargs)
+
+    # 2. model
+
+    model = torchfcn.models.FCN32s(n_class=21)
+    start_epoch = 0
+    start_iteration = 0
+    if args.resume:
+        checkpoint = torch.load(args.resume)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        start_epoch = checkpoint['epoch']
+        start_iteration = checkpoint['iteration']
+    if cuda:
+        model = model.cuda()
+
+    # 3. optimizer
+
+    optim = torch.optim.SGD(
+        [
+            {'params': get_parameters(model, bias=False)},
+            {'params': get_parameters(model, bias=True),
+             'lr': args.lr * 2, 'weight_decay': 0},
+        ],
+        lr=args.lr,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay)
+    
+    if args.resume:
+        optim.load_state_dict(checkpoint['optim_state_dict'])
+
+    trainer = torchfcn.Trainer(
+        cuda=cuda,
+        model=model,
+        optimizer=optim,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        out=args.out,
+        max_iter=args.max_iteration,
+        interval_validate=4000,
+    )
+    trainer.epoch = start_epoch
+    trainer.iteration = start_iteration
+    trainer.train()
+
+
+if __name__ == '__main__':
+    start_time = time.time()  # Capture the start time
+    main()
+    end_time = time.time()  # Capture the end time
+    elapsed_time = end_time - start_time  # Calculate the elapsed time
+    print(f"Total execution time: {elapsed_time:.2f} seconds")
